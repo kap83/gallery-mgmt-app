a6b55d1d8ad95b7c4d549333ed32ec53489f0a54
Karen Polanco <polanco.k@gmail.com>
2023-10-27 16:36:26 -0400

    password auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies 
+    wrap_parameters format: []
+
+    # @current_user = User.find_by(id: session[:user_id])
 
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,22 +3,27 @@ class UsersController < ApplicationController
     
     def create
         user = User.find_by(username: params[:username])
+       if user&.authenticate(params[:password])
         session[:user_id] = user.id
-        render json: user
+        render json: user, status: :created
+       else
+        render json: {error: "Invalid username or password"}, status: :unauthorized
+       end
     end
 
-    def show 
-        user = @current_user
-        if user
-            render json: user
-        else
-            render json: {error: "Please Sign In"}, status: :unauthorized
-        end
-    end
+    # def show 
+    #     user = @current_user
+    #     if user
+    #         render json: user
+    #     else
+    #         render json: {error: "Please Sign In"}, status: :unauthorized
+    #     end
+    # end
 
-    def destroy
-        session.delete :user_id
-        head :no_content 
-    end
+    # def destroy
+    #     session.delete :user_id
+    #     head :no_content 
+    # end
+
 
 end
