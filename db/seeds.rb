ffc051b2b2eca95cf833371f59684e35da02c740
Karen Polanco <polanco.k@gmail.com>
2024-05-24 19:00:29 -0400

    removed 2nd readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,26 +7,24 @@ user2 = User.create(name: 'Alec Hardison', username: "geek", password: "password
 user1.avatar.attach(io: File.open(Rails.root.join('db/avatars/sophie.jpg')), filename: 'sophie.jpg')
 user2.avatar.attach(io: File.open(Rails.root.join('db/avatars/hardison.jpg')), filename: 'hardison.jpg')
 
-Exhibition.create(id: 1, title: 'Animals', gallery: 'A', start_date: '2024-10-01', end_date: '2025-06-30', user_id: 1 )
-Exhibition.create(id: 2, title: 'Women in Art', gallery: 'B', start_date: '2024-02-13', end_date: '2024-09-01', user_id: 2 )
-
+Exhibition.create(id: 3, title: 'Animals', gallery: 'A', start_date: '2024-10-01', end_date: '2025-06-30', user_id: 1 )
+Exhibition.create(id: 4, title: 'Women in Art', gallery: 'B', start_date: '2024-02-13', end_date: '2024-09-01', user_id: 2 )
 
 Artist.create(name: 'Paul Klee', date_of_birth: '1879-12-18')
 Artist.create(name: 'Mary Cassatt', date_of_birth: '1844-05-22')
 Artist.create(name: 'Franz Marc', date_of_birth: '1880-02-08')
 
 
-
 art1 = Artwork.create(title: 'Fleeing Ghost', medium: 'oil on canvas', artist_id: 1)
 art2 = Artwork.create(title: 'Strange Glance', medium: 'oil on canvas', artist_id: 1)
 art3 = Artwork.create(title: 'Diana', medium: 'oil on canvas', artist_id: 1)
 art4 = Artwork.create(title: 'Castle and Sun', medium: 'oil on canvas', artist_id: 1)
-art5 = Artwork.create(title: 'Tea', medium: 'oil on canvas', artist_id: 2, exhibition_id: 2)
-art6 = Artwork.create(title: 'Woman Bathing', medium: 'color aquatint, with drypoint from three plates, on off-white laid paper', artist_id: 2, exhibition_id: 2)
-art7 = Artwork.create(title: "The Child's Bath", medium: 'oil on canvas', artist_id: 2, exhibition_id: 2)
-art8 = Artwork.create(title: "Blue Horses", medium: 'oil on canvas', artist_id: 3, exhibition_id: 1)
-art9 = Artwork.create(title: "Yellow Cow", medium: 'oil on canvas', artist_id: 3, exhibition_id: 1)
-art10 = Artwork.create(title: "Tiger", medium: 'color woodcut print on paper', artist_id: 3, exhibition_id: 1)
+art5 = Artwork.create(title: 'Tea', medium: 'oil on canvas', artist_id: 2, exhibition_id: 4)
+art6 = Artwork.create(title: 'Woman Bathing', medium: 'color aquatint, with drypoint from three plates, on off-white laid paper', artist_id: 2, exhibition_id: 4)
+art7 = Artwork.create(title: "The Child's Bath", medium: 'oil on canvas', artist_id: 2, exhibition_id: 4)
+art8 = Artwork.create(title: "Blue Horses", medium: 'oil on canvas', artist_id: 3, exhibition_id: 3)
+art9 = Artwork.create(title: "Yellow Cow", medium: 'oil on canvas', artist_id: 3, exhibition_id: 3)
+art10 = Artwork.create(title: "Tiger", medium: 'color woodcut print on paper', artist_id: 3)
 
 art1.paintings.attach(io: File.open(Rails.root.join('db/artworks/Klee_fleeing_ghost.jpg')), filename: 'Klee_fleeing_ghost.jpg')
 art2.paintings.attach(io: File.open(Rails.root.join('db/artworks/Klee_strange_glance.jpg')), filename: 'Klee_strange_glance.jpg')
